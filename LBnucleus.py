88088900642dc6ada32422a3cbf6b059af82765b
lb425 <lane.broadbent@gmail.com>
2015-07-09 16:06:34 -0600

    allow for the preservation of source IP

--- LBnucleus.py
+++ LBnucleus.py
@@ -11,6 +11,10 @@ import errno
 import threading
 
 
+##For source preservation
+##Wrap in if for type 2 ports . . . 
+from scapy.all import *
+
 run=True
 debug=False
 coldStart=False
@@ -27,7 +31,9 @@ dataBuffers=[]
 #####
 
 ##Ports list entries follow format of [UDP port, n] where n mean to sent 1/n packets where n is an integer, and n>0
-ports=[[5004,1], [5005,1], [5006,1], [5007,1], [5008,1], [5009,1]]
+##Format: [<port>, <decimation factor>, <output type>]
+##output types: 0: disabled   1:normal   2: preverse src address
+ports=[[5004,1,1], [5005,1,1], [5006,1,1], [5007,1,2], [5008,1,1], [5009,1,1]]
 sockets=[]
 destinations=["172.22.22.130", "172.22.22.131", "172.22.22.132", "172.22.22.133", "172.22.22.134", "172.22.22.135", "172.22.22.136", "172.22.22.137", "172.22.22.138", "172.22.22.139"]
 perDestEnable=[]
@@ -112,7 +118,7 @@ class inputHandler(threading.Thread):
                                         data, addr = self.socket.recvfrom(bufferSize)
                                 for j in 3*range(len(destinations)):
                                         data, addr = self.socket.recvfrom(bufferSize) # buffer size is "bufferSize" bytes
-                                        dataBuffers[self.streamID].append(data)
+                                        dataBuffers[self.streamID].append([data, addr])
 
                 while run:
                         #DECIMATION!!!!                         Divisor
@@ -121,7 +127,7 @@ class inputHandler(threading.Thread):
 
                         #gather data
                         data, addr = self.socket.recvfrom(bufferSize) # buffer size is "bufferSize" bytes
-                        dataBuffers[self.streamID].append(data)
+                        dataBuffers[self.streamID].append([data, addr])
 
 class outputHandler(threading.Thread):
         def __init__(self, streamID):
@@ -140,10 +146,31 @@ class outputHandler(threading.Thread):
                                 for i in range(len(destinations)):
                                         if perDestEnable[i] == 1:
                                                 tmp=destinations[i]
-                                                socket.sendto(dataBuffers[self.streamID].pop(), (destinations[i], ports[self.streamID][0]))
+                                                socket.sendto(dataBuffers[self.streamID].pop()[0], (destinations[i], ports[self.streamID][0]))
                         else:
                                 time.sleep(.1)
 
+class outputHandlerSRC(threading.Thread):
+        def __init__(self, streamID):
+                threading.Thread.__init__(self)
+                self.streamID=streamID
+        def run(self):
+                global dataBuffers
+                global destinations
+                global perDestEnable
+                global run
+                global ports
+                global sockets
+                socket = sockets[self.streamID]
+                while run:
+                        if len(dataBuffers[self.streamID])>len(destinations):
+                                for i in range(len(destinations)):
+                                        if perDestEnable[i] == 1:
+                                                tmp=destinations[i]
+                                                tmp2 = dataBuffers[self.streamID].pop()
+                                                send(IP(dst=destinations[i], src=tmp2[1][0])/UDP(sport=ports[self.streamID][0], dport=ports[self.streamID][0])/tmp2[0], verbose=False)
+                        else:
+                                time.sleep(.1)
 
 
 
@@ -155,7 +182,13 @@ for i in range(len(destinations)):
 time.sleep(5)
 
 for i in range(len(ports)):
-        newthread = outputHandler(i)
+        if ports[i][2] == 0:
+            print "b"
+        elif ports[i][2] == 1:
+            newthread = outputHandler(i)
+        elif ports[i][2] == 2:
+            newthread = outputHandlerSRC(i)
+#        newthread = outputHandler(i)
         newthread.start()
         threads.append(newthread)
 
